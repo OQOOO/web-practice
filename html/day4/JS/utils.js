0ec2fb7874a16f04207db610bffbbb4456a1b10c
Lim KiHong <igh0730@naver.com>
2023-05-09 13:06:13 +0900

    javascriptInputTest

---
+++ html/day4/JS/utils.js
@@ -0,0 +1,15 @@
+function hi() {
+  //   for (i = 0; i < 5; ++i) {
+  //     document.write(i + "<br>");
+  //   }
+  console.log("Hi!");
+}
+
+function sum(a, b) {
+  let result = a + b;
+  console.log(result);
+}
+
+function total(x, y) {
+  return x + y;
+}
